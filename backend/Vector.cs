7bb116da81893261ece09474d88c82480473377e
Muhammad Bilal Naseer <mbnaseer@mun.ca>
2024-11-18 21:33:15 -0330

    Update Vector.cs

--- backend/Vector.cs
+++ backend/Vector.cs
@@ -1,1 +1,24 @@
+using System;
+
+namespace ObjectSimulation
+{
+    public class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.X + b.X, a.Y + b.Y);
+        public static Vector operator -(Vector a, Vector b) => new Vector(a.X - b.X, a.Y - b.Y);
+        public static Vector operator *(Vector a, double scalar) => new Vector(a.X * scalar, a.Y * scalar);
+        public static Vector operator /(Vector a, double scalar) => new Vector(a.X / scalar, a.Y / scalar);
+
+        public double Magnitude() => Math.Sqrt(X * X + Y * Y);
+    }
+}
 
